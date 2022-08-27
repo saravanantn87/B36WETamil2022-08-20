6a7808ed3593038c12d3b664ff92bf20a9aaf249
saravanantn87 <111057164+saravanantn87@users.noreply.github.com>
2022-08-27 12:53:55 +0530

    Add files via upload

---
+++ Jason.js
@@ -0,0 +1,22 @@
+var arr =[1, 2, 3, 4, 5]; // for  -- it will print the entire number. one by one.
+
+for (var i =0; i <arr.length; i++) {
+
+
+console.log(arr[i]);
+}
+
+
+var arr = [1, 2, 3, 4, 5]; //  for of -- it will print the entire number.
+
+for (var  s of arr ){
+    console.log(s)
+}
+
+var arr = [1, 2, 3, 4, 5]; // for in - it will print only index.
+
+for (var s in arr) {
+    console.log(s);
+}
+
+
---
+++ resume.js
@@ -0,0 +1,12 @@
+const myresume={
+   
+    name: "Saran",
+    degree:"B.B.A",
+    dob : "20-04-1986"
+    address: "Chennai",
+postalCode: 600074,
+state: "Tamilnadu",
+country: "India",
+};
+  console.log(myresume);
+  console.log(myresume.name);
